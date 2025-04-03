3e45e791babb40356547077c64f3238851ab44c0
Totiii14 <toti.gamer14@gmail.com>
2025-04-03 10:08:45 -0300

    ExperienceBar

---
+++ Assets/Scripts/Player/ExperienceManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ExperienceManager : MonoBehaviour
+{
+    [Header("Experience")]
+    [SerializeField] AnimationCurve experienceCurve;
+
+    int currentLevel, totalExperience;
+    int previousLevelsExperience, nextLevelsExperience;
+
+    [Header("Interface")]
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] TextMeshProUGUI experienceText;
+    [SerializeField] Image experienceFill;
+
+    private void Start()
+    {
+        UpdateLevel();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            AddExperience(5);
+        }
+    }
+
+    public void AddExperience(int amount)
+    {
+        totalExperience += amount;
+        CheckForLevelUp();
+        UpdateInterface();
+    }
+
+    void CheckForLevelUp()
+    {
+        if (totalExperience >= nextLevelsExperience)
+        {
+            currentLevel++;
+            UpdateLevel();
+        }
+    }
+
+    void UpdateLevel()
+    {
+        previousLevelsExperience = (int)experienceCurve.Evaluate(currentLevel);
+        nextLevelsExperience = (int)experienceCurve.Evaluate(currentLevel + 1);
+        UpdateInterface();
+    }
+
+    void UpdateInterface()
+    {
+        int start = totalExperience - previousLevelsExperience;
+        int end = nextLevelsExperience - previousLevelsExperience;
+
+        levelText.text = currentLevel.ToString();
+        experienceText.text = start + " exp / " + end + " exp";
+        experienceFill.fillAmount = (float)start / (float)end;
+    }
+}
