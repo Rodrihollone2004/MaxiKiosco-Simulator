a1f3b1b581b7c6ed3bead6ab55560f7b2bf52be2
Rodrihollone2004 <rodri.g.fretes@gmail.com>
2025-07-05 17:39:07 -0300

    UI Products
    Clear canvas of prefab products
    Add a UIProductManager for show the info of products
    Now is only one Canvas that is the main Canvas

---
+++ Assets/Scripts/Managers/ProductUIManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ProductUIManager : MonoBehaviour
+{
+    public static ProductUIManager Instance { get; private set; }
+
+    [SerializeField] private GameObject hintUI;
+    [SerializeField] private TMP_Text amountText;
+    [SerializeField] private TMP_Text priceText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void ShowInfo(string name, int price, int amount)
+    {
+        HideInfo();
+
+        amountText.text = $"Restantes: {amount}";
+        priceText.text = $"Precio: {price}";
+        hintUI.SetActive(true);
+        Debug.Log($"Interactuando con {name} (${price})");
+
+        StartCoroutine(HideInfoAfterDelay(3));
+    }
+
+    public void HideInfo()
+    {
+        hintUI.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    private IEnumerator HideInfoAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        hintUI.SetActive(false);
+    }
+}
--- Assets/Scripts/Products/ProductInteractable.cs
+++ Assets/Scripts/Products/ProductInteractable.cs
@@ -7,14 +7,9 @@ public class ProductInteractable : MonoBehaviour, IInteractable
     public static ProductInteractable currentHintOwner;
 
     [SerializeField] private Product _productData;
-    [SerializeField] private float _highlightWidth = 1.03f;
-    [SerializeField] private GameObject amountHintUI;
-    [SerializeField] private TMP_Text amountText;
-    [SerializeField] private TMP_Text priceText;
-    public Product ProductData => _productData;
-
     [SerializeField] int currentAmountProduct;
 
+    public Product ProductData => _productData;
     public bool CanBePickedUp => false;
 
     public void Initialize(Product productData)
@@ -25,21 +20,7 @@ public class ProductInteractable : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        if (currentHintOwner != null && currentHintOwner != this)
-        {
-            currentHintOwner.amountHintUI.SetActive(false);
-            currentHintOwner.StopAllCoroutines();
-        }
-
-        amountText.text = $"Restantes: {currentAmountProduct}";
-        priceText.text = $"Precio: {_productData.Price}";
-        amountHintUI.SetActive(true);
-        Debug.Log($"Interactuando con {_productData.Name} (${_productData.Price})");
-
-        if (currentHintOwner == null)
-            StartCoroutine(HideSummaryAfterDelay(3));
-
-        currentHintOwner = this;
+        ProductUIManager.Instance.ShowInfo(_productData.Name, _productData.Price, currentAmountProduct);
     }
 
     public void SubtractAmount()
@@ -51,15 +32,6 @@ public class ProductInteractable : MonoBehaviour, IInteractable
     {
         if (currentAmountProduct <= 0)
             Destroy(gameObject);
-    }
-
-    private IEnumerator HideSummaryAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-
-        amountHintUI.SetActive(false);
-        currentHintOwner = null;
-
     }
 
     public void Highlight()
