ef3dea26c260cd9c8f297a606e3b39aff36a359e
xNemesis47x <101485936+xNemesis47x@users.noreply.github.com>
2025-07-11 12:32:55 -0300

    Add music list, back and next

--- Assets/Scripts/InteractableObjects/Radio.cs
+++ Assets/Scripts/InteractableObjects/Radio.cs
@@ -4,6 +4,9 @@ public class Radio : MonoBehaviour, IInteractable
 {
     private AudioSource _audioSource;
 
+    [SerializeField] private AudioClip[] songs;
+    private int currentSongIndex = 0;
+
     public bool CanBePickedUp => false;
     public bool IsHeld { get; private set; } = false;
 
@@ -11,20 +14,59 @@ public class Radio : MonoBehaviour, IInteractable
     {
         _audioSource = GetComponent<AudioSource>();
     }
+
     private void Start()
     {
+        if (songs.Length > 0)
+        {
+            _audioSource.clip = songs[currentSongIndex];
+        }
         _audioSource.Stop();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            NextSong();
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            PreviousSong();
+        }
     }
 
     public void Interact()
     {
-        if (!_audioSource.isPlaying)
-            _audioSource.Play();
-        else
+        if (_audioSource.clip == null) return;
+
+        if (_audioSource.isPlaying)
             _audioSource.Pause();
+        else
+            _audioSource.Play();
     }
 
-    public void Highlight(){}
+    private void NextSong()
+    {
+        if (songs.Length == 0) return;
 
-    public void Unhighlight(){}
+        currentSongIndex = (currentSongIndex + 1) % songs.Length;
+        _audioSource.clip = songs[currentSongIndex];
+        _audioSource.Play();
+    }
+
+    private void PreviousSong()
+    {
+        if (songs.Length == 0) return;
+
+        currentSongIndex--;
+        if (currentSongIndex < 0)
+            currentSongIndex = songs.Length - 1;
+
+        _audioSource.clip = songs[currentSongIndex];
+        _audioSource.Play();
+    }
+
+    public void Highlight() { }
+    public void Unhighlight() { }
 }
