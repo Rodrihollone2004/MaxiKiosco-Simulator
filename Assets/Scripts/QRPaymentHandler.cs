288fee910aaffead909c32b701f52e16b38b445b
Rodrihollone2004 <rodri.g.fretes@gmail.com>
2025-09-09 15:49:21 -0300

    QR buttons implemented

--- Assets/Scripts/QRPaymentHandler.cs
+++ Assets/Scripts/QRPaymentHandler.cs
@@ -3,28 +3,42 @@ using TMPro;
 
 public class QRPaymentHandler : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField amountInput;
     [SerializeField] private CashRegisterInteraction cashRegisterInteraction;
     [SerializeField] private GameObject qrPaymentUI;
     [SerializeField] private PlayerCam playerCam;
+    [SerializeField] private TMP_Text qrPaymentText;
 
     private Client currentClient;
 
     public void SetupQRPayment(Client client)
     {
         currentClient = client;
+        qrPaymentText.text = "";
         qrPaymentUI.SetActive(true);
+    }
+
+    public void AddNumber(int number)
+    {
+        qrPaymentText.text += $"{number}";
+    }
+
+    public void SubtractNumber()
+    {
+        if (!string.IsNullOrEmpty(qrPaymentText.text))
+        {
+            qrPaymentText.text = qrPaymentText.text.Remove(qrPaymentText.text.Length - 1);
+        }
     }
 
     public void ConfirmQRPayment()
     {
-        if (currentClient != null && int.TryParse(amountInput.text, out int amount))
+        if (currentClient != null && int.TryParse(qrPaymentText.text, out int amount))
         {
             if (amount == currentClient.totalCart)
             {
                 cashRegisterInteraction.ProcessQRPayment(currentClient, amount);
                 CancelQRPayment();
-                amountInput.text = "";
+                qrPaymentText.text = "";
             }
             else
             {
